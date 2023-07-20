73c2cb5beb2c9acc8a399acc8a0680ecc5dc2480
Minisiia <ar_tik@yahoo.com>
2023-07-20 21:14:25 +0300

    7. Spring 5. Junit

---
+++ src/test/java/com/example/genshinimpactjunit5springboot/repositories/RegionsRepositoryTest.java
@@ -0,0 +1,23 @@
+package com.example.genshinimpactjunit5springboot.repositories;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class RegionsRepositoryTest {
+    @Autowired
+    RegionsRepository regionsRepository;
+
+    @Test
+    void getGenshinHeroesNamesFromSomeLocationAndSomeWeapon() {
+        String location = "Some location";
+        List<String> list = regionsRepository.getGenshinHeroesNamesFromSomeLocationAndSomeWeapon(location);
+        assertNotNull(list);
+    }
+}
